a67ef2870c072bc7f64a3076046631e705d5d433
StephenNnamani <sir.stephennnamani@gmail.com>
2022-02-02 13:51:42 +0000

    New updates

--- Server/app.js
+++ Server/app.js
@@ -6,9 +6,26 @@ const mongoose          = require('mongoose').mongoDB
 const port              = '3000'
 const app               = express();
 
-app.get('/',  (req, res, err) =>{
-    res.send({title: "hello world"})
+let name = "chinaemenma"
+let hotel = "Golden Royal"
+let visa = `You have been issued a 5 years H1B visa ${23 + 7}`
+app.get('/',  (req, res) =>{
+    res.send(`My name is ${name}`)
 })
+
+// Mongoose connect
+mongoose.connect(process.env.DB_CONNECTION)
+
+//Routes are here
+const flightRoute = require("./routes/flights");
+const hotelRoute  = require("./routes/hotels")
+const visaRoute   = require("./routes/visa")
+
+app.use('/flights', flightRoute)
+app.use('/hotels', hotelRoute)
+app.use('/visa', visaRoute)
+
+
 
 app.listen(port, () => {
     console.log(`The Smart Flight Booking is listening on port ${port}`)
